e3710c3d298a3637223564dfbaee1051078a2f2f
nezaicev <nezaicev@yandex.ru>
2020-08-17 16:35:02 +0300

    Упростил код, исправил структуру скрипта

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import requests
 import argparse
 import os
-
 from urllib.parse import urlparse
 from dotenv import load_dotenv
-load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
 URL_GENERATE_BITLINK = 'https://api-ssl.bitly.com/v4/bitlinks'
 URL_COUNT_CLICKS = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'
 
 
 def shorten_link(link, headers):
-    bitlink = {'long_url': link}
-    response = requests.post(URL_GENERATE_BITLINK, headers=headers, json=bitlink)
+    body_request = {'long_url': link}
+    response = requests.post(URL_GENERATE_BITLINK, headers=headers, json=body_request)
+    response.raise_for_status()
     return response.json()
 
 
@@ -21,19 +19,20 @@ def count_clicks(bitlink, headers):
     params = {'unit': 'day', 'units': -1}
     url = URL_COUNT_CLICKS.format(bitlink)
     response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
     return response.json()['total_clicks']
 
 
 def main():
+
     parser = argparse.ArgumentParser(
-        description="Описание"
+        description="Скрипт позволяет генерировать короткие ссылки и и получать информацию о количестве кликов "
     )
     parser.add_argument('link', help='Ссылка на ресурс')
-    headers = {'Authorization': 'Bearer {}'.format(TOKEN)}
+    headers = {'Authorization': 'Bearer {}'.format(BITLY_GENERIC_ACCESS_TOKEN)}
     link_parse = urlparse(parser.parse_args().link)
 
     try:
-        requests.get(link_parse.geturl()).raise_for_status()
         if link_parse.netloc.startswith('bit.ly'):
             id_bitlink = link_parse.netloc + link_parse.path
             print('По вашей ссылке прошли:', count_clicks(id_bitlink, headers), 'раз(а)')
@@ -45,4 +44,6 @@ def main():
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    BITLY_GENERIC_ACCESS_TOKEN = os.getenv('BITLY_GENERIC_ACCESS_TOKEN')
     main()
